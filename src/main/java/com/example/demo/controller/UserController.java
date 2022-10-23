13d5c551576c074c9ab399673abce3e69e59391f
Wooho <simaro0816@gamil.com>
2022-10-23 17:02:41 +0900

    RestController Add

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/user/getPort")
+	public String getPort(@Value("${server.port}") String port) {
+		return "User 서비스의 기본 동작 Port: {" + port + "}";
+	}
+
+}
